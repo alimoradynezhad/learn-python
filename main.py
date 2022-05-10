ae0d06e7a3cd80f8a2160e5338c83e5a4d7e53e9
alimoradynezhad <asmnbala@gmail.com>
2022-05-10 17:59:44 +0430

    data conversion

--- main.py
+++ main.py
@@ -1,39 +1,15 @@
-print("--------- tupleنوع داده -------")
-# مثل لیست فقط در دیتا بیس از تاپل استفاده میشه وبه جای براکت از پرانتز استفاده میکنه
-tuple1 = (1, 2, 5, "ali")
-print(tuple1)
-print(type(tuple1))
-print(tuple1[1])
+str_to_int = int('20')
+int_to_str = str(11)
 
 
-print("-----------نوع دادهset ----------------")
-#ایندکسی نیست
-set1 ={1, 2, 5, "ali", 15, "reza"}
-set2 ={'hamed', 4, 5, 12}
-print(set1)
-print('ali' in set1)
-print(set2 | set1)
-print(set1 > set2)
-print(set1 < set2)
-print(set1 - set2)
-print(set1 & set2)# هردو موجوده
-print(len(set1))
-set1.add('ahmad')
-print(set1)
 
+list = ['10', '12', '13']
+sum = list[0] + list[1]
+print(sum)
+a = int(list[0])
+b = int(list[1])
+sum2 =a+b
+print(sum2)
 
-print("---------------نوع داده dictionary---------------")
-capitals = {'iran':'tehran',
-            'france':'paris',
-            'italy':'rom'}
-capitals2 ={'germany':'berlin',
-            'uk':'london'}
-print(capitals)
-print(capitals['iran'])
-capitals['spain'] = 'madrid'
-print(capitals)
-print('iran' in capitals)
-capitals.update(capitals2)#add two dictionary
-print(capitals)
-'shiraz' in capitals.keys()
-print(capitals.keys())
+str_to_int = int('20')
+int_to_str = str(11)
