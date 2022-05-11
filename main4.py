509809c0d6d553f49103aad2143dda4c1c971311
alimoradynezhad <asmnbala@gmail.com>
2022-05-11 21:20:20 +0430

    if else and scope in python

---
+++ main4.py
@@ -0,0 +1,19 @@
+val = 21
+if val == 20 :
+    print("val ==20" )
+elif val >10:
+    print("bozorgtar az 10")
+else:
+    print("false")
+
+lst = [1, 2, 3]
+if len(lst) != 0:#اگر طول لیست نامساوی با صفر
+    print(lst[0])
+
+lst = [1, 2, 3]
+if lst:# اگر طول لیست نامساوی با صفر
+    print(lst[0])
+
+lst = ['ali', 'reza', 'hamed']
+if lst:# اگر طول لیست نامساوی با صفر
+    print(lst[1])
