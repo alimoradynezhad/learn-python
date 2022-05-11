79602fcbdbc71a6b592df7331c1e107fb01cd776
alimoradynezhad <asmnbala@gmail.com>
2022-05-11 17:19:11 +0430

    strip | lower | set | sort char in python

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-# if تک خطی
-val1 = 10
-val2 = 20  if val1 == 1 else 10#اگر val1 مقدارش 1 بود که هیچ اگر نه val2 را با 10 مقدار دهی کن
-print(val2)
+import os
+with open("myfile.txt", "r") as f:
+    deta =f.readlines()
+    print(deta)
+    print("-----------")
+    word = deta[0]
+    word.strip()#حذف چیزهای اضافه از استرینگ مثل \n
+    word.lower()#کوچک کردن کاراکتر
+    set(deta)#حذف تکراری ها
+    print(deta)
+    print(word)
+    clean =[sorted(set(word.strip().lower())) for word in deta]#sort baray moratab kardan
+    print(clean)
 
-val1 = 10
-val2 = 20  if val1 == 1 else 5#اگر val1 مقدارش 1 بود که هیچ اگر نه val2 را با 5 مقدار دهی کن
-print(val2)
 
-sqr1 = [i**2 for i in range(10)]#از صفر تا 10 را به توان 2 برسان
-print(sqr1)
-sqr = [i**3 if i % 3 == 0 else 10 for i in range(10, 30)]#از 10 تا 30 را به توان 3 برسان اگر باقیمانده تقسیم بر سه اش 0 شد در غیر اینصورت 10 بزار
-print(sqr)
+
+
