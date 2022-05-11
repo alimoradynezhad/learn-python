c99d6ee881e85bedb1a770791d24198863364ccf
alimoradynezhad <asmnbala@gmail.com>
2022-05-11 21:34:02 +0430

    convert deta

---
+++ converdeta.py
@@ -0,0 +1,14 @@
+str_to_int = int('20')
+int_to_str = str(11)
+
+
+list = ['10', '12', '13']
+sum = list[0] + list[1]
+print(sum)
+a = int(list[0])
+b = int(list[1])
+sum2 =a+b
+print(sum2)
+
+str_to_int = int('20')
+int_to_str = str(11)
